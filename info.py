7e11528b11401e20fbe1fdeb969cd8f11796bfd6
817100 <128728674+817100@users.noreply.github.com>
2023-10-09 16:37:34 +0530

    Update info.py

--- info.py
+++ info.py
@@ -30,7 +30,7 @@ VRFY_IMG = environ.get("VRFY_IMG", "https://graph.org/file/ca19ea1ba45d5ae9cc0ec
 
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1737202396').split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001768075000').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 PREMIUM_USER = [int(user) if id_pattern.search(user) else user for user in environ.get('PREMIUM_USER', '').split()]
